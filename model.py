7c720f5dacc6191b336f4fe556d004d1dfaec412
Sonia Tyburczy <styburczy.business@gmail.com>
2025-04-20 11:34:03 -0400

    best_link added

--- model.py
+++ model.py
@@ -10,5 +10,29 @@ class WikiNavigator:
 
     def get_embeddings(self, s):
         return self._embedder.encode(s, convert_to_tensor=True)
+    
+    def similarity(self, target, comparison):
+        target_embedding = self.get_embeddings(target)
+        
+        if not isinstance(comparison, list):
+            comparison = [comparison]
 
-#hits = util.semantic_search(query_embeddings, corpus_embeddings, top_k=3)
+        comparison_embeddings = [self.get_embeddings(item) for item in comparison]
+        results = util.semantic_search(target_embedding, comparison_embeddings, top_k=1)
+        return results
+
+    def best_link(self):
+        results = self.similarity(self.target_link, self.links)
+        index = results[0][0].get('corpus_id')
+        link = self.links[index]
+        similarity = results[0][0].get('score')
+        return link, similarity
+
+link = 'Hypertension'
+target = 'Eggs'
+links = ['Diet', 'Heart attack', 'Doctor']
+
+wiki = WikiNavigator(link, target, links)
+print(wiki.best_link())
+
+#hits = semantic_search(query_embeddings, corpus_embeddings, top_k=3)
