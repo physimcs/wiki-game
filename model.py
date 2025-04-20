1acd8fea81d0036e59a47263205123879ecdeb37
Sonia Tyburczy <styburczy.business@gmail.com>
2025-04-20 11:00:29 -0400

    Model changes

--- model.py
+++ model.py
@@ -1,10 +1,14 @@
-from sentence_transformers import SentenceTransformer
-model = SentenceTransformer("multi-qa-mpnet-base-cos-v1")
+import torch 
+from sentence_transformers import SentenceTransformer, util
 
 class WikiNavigator:
     def __init__(self, current_link, target_link, possible_links):
         self.current_link = current_link
         self.target_link = target_link
-        self.possible_links = possible_links
+        self.links = possible_links
+        self._embedder = SentenceTransformer("all-MiniLM-L6-v2")
 
-# current_link, target_link, possible_links
+    def get_embeddings(self, s):
+        return self._embedder.encode(s, convert_to_tensor=True)
+
+#hits = util.semantic_search(query_embeddings, corpus_embeddings, top_k=3)
